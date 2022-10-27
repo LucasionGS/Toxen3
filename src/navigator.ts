a36dd2afe89907da4a81d02d02a022758a3bf24a
Lucasion <40472939+LucasionGS@users.noreply.github.com>
2022-10-27 10:00:55 +0200

    I regret my change go back

--- src/navigator.ts
+++ src/navigator.ts
@@ -1,19 +1,19 @@
-interface INavigator extends globalThis.Navigator {
-  mediaSession: {
-    metadata: IMediaMetadata;
-    setActionHandler(event: "nexttrack", callback: () => void): void;
-    setActionHandler(event: string, callback: () => void): void;
-  };
-}
+// interface INavigator extends globalThis.Navigator {
+//   mediaSession: {
+//     metadata: IMediaMetadata;
+//     setActionHandler(event: "nexttrack", callback: () => void): void;
+//     setActionHandler(event: string, callback: () => void): void;
+//   };
+// }
 
-export declare class IMediaMetadata {
-  constructor(object: {
-    title: string;
-    artist: string;
-    album: string;
-    artwork: { src: string, sizes: string, type: string }[];
-  });
-}
+// export declare class IMediaMetadata {
+//   constructor(object: {
+//     title: string;
+//     artist: string;
+//     album: string;
+//     artwork: { src: string, sizes: string, type: string }[];
+//   });
+// }
 
-export default globalThis.navigator as INavigator;
-export const MediaMetadata = (globalThis as any).MediaMetadata as typeof IMediaMetadata;
+// export default globalThis.navigator as INavigator;
+// export const MediaMetadata = (globalThis as any).MediaMetadata as typeof IMediaMetadata;
